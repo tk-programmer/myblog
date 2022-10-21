de7bf4c376793dce897c9bacc4a764efd44eace8
tkj <1341835590@qq.com>
2022-10-21 15:48:45 +0800

    更新config.js

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,8 +1,3 @@
-import { defineUserConfig } from 'vuepress'
-
-export default defineUserConfig({
-  lang: 'zh-CN',
-  title: '你好， VuePress ！',
-  description: '这是我的第一个 VuePress 站点',
-  base: '/myblog/'
-})
+module.exports = {
+    base: "/myblog/"
+}
